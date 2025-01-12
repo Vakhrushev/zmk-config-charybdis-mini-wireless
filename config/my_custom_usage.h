1eb3fbbca4d1dca6da05f764ceb6af33f495d5f2
Vladislav Vakhrushev <vladislav.vakhrushev@tabby.ai>
2025-01-12 20:43:31 +0400

    ZMK-7

--- config/my_custom_usage.h
+++ config/my_custom_usage.h
@@ -6,6 +6,6 @@
 #define MY_VENDOR_USAGE_PAGE  0xFF00
 
 // Define a few custom usages. The high 16 bits is the usage page; low 16 bits is the usage ID.
-#define MY_KEY_1  HID_USAGE(MY_VENDOR_USAGE_PAGE, 0x01)
-#define MY_KEY_2  HID_USAGE(MY_VENDOR_USAGE_PAGE, 0x02)
-#define MY_KEY_3  HID_USAGE(MY_VENDOR_USAGE_PAGE, 0x03)
+#define MY_KEY_1  ZMK_HID_USAGE(MY_VENDOR_USAGE_PAGE, 0x01)
+#define MY_KEY_2  ZMK_HID_USAGE(MY_VENDOR_USAGE_PAGE, 0x02)
+#define MY_KEY_3  ZMK_HID_USAGE(MY_VENDOR_USAGE_PAGE, 0x03)
