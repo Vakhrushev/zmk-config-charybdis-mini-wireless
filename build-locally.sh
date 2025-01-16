5d03fd2549df44357200bc405b305bfdee4cb2ec
Vladislav Vakhrushev <vladislav.vakhrushev@tabby.ai>
2025-01-16 12:17:59 +0400

    zmk

--- build-locally.sh
+++ build-locally.sh
@@ -11,7 +11,6 @@ build_and_copy () {
     west build \
         -b nice_nano_v2 -- \
         -DSHIELD=charybdis_$side \
-        -S zmk-usb-logging \
         -DZMK_CONFIG="/home/erenatas/GitRepos/zmk-config-charybdis-mini-wireless/config"
 
     cp "./build/zephyr/zmk.uf2" "$CURRENT_DIR/build/charybdis_$side.uf2"
